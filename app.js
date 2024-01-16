ee77a6d9e4e51f138dc0bcaa2bbaff95bbf35e06
Xuggi <godannyyy@gmail.com>
2024-01-16 09:50:54 +0100

    first chalk

---
+++ app.js
@@ -0,0 +1,7 @@
+import chalk from "chalk"
+
+
+console.log(chalk.bgYellow('Why did the chicken cross the road'));
+console.log(chalk.bgBlue('Toooo becoma a chicken nugget'));
+console.log(chalk.bgRed('HAHAHAHAH'));
+
