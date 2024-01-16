5167aba584023e994e09baf2d8f44fb6bcaa50e8
Xuggi <godannyyy@gmail.com>
2024-01-16 10:04:40 +0100

    second joke update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import chalk from "chalk"
 
+console.log(chalk.yellow(chalk.red('Why') + ' ' + chalk.green('did') + ' ' + chalk.blue('the') + ' ' + chalk.cyan('chicken') + ' ' + chalk.magenta('cross') + ' ' + chalk.yellow('the') + ' ' + chalk.green('road')));
+console.log(chalk.blue(chalk.yellow('Toooo') + ' ' + chalk.cyan('become') + ' ' + chalk.red('a') + ' ' + chalk.magenta('chicken') + ' ' + chalk.yellow('nugget')));
+console.log(chalk.bgRed(chalk.yellow('HAHAHAHAH')));
 
-console.log(chalk.bgYellow('Why did the chicken cross the road'));
-console.log(chalk.bgBlue('Toooo becoma a chicken nugget'));
-console.log(chalk.bgRed('HAHAHAHAH'));
 
